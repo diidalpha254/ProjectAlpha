7ee9e97afca772d343530b26305c71ff35f769f6
diidalpha254 <borudiid064@gmail.com>
2026-08-20 09:44:00 +0300

    Fix import formatting in notifications.py

--- app/features/notifications.py
+++ app/features/notifications.py
@@ -9,10 +9,10 @@ import uuid
 import threading
 from collections import deque
 
-12  from core.types import Notification
-13  from core.constants import NotificationType
-14  from core.logger import get_logger
-15  from data.event_bus import event_bus
+from core.types import Notification
+from core.constants import NotificationType
+from core.logger import get_logger
+ from data.event_bus import event_bus
 
 
 logger = get_logger(__name__)
